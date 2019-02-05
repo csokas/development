08d4ea1cbb28bff1f943f8d21309d08b39353155
Csókás László <cslaszlo01@gmail.com>
2019-02-05 19:34:00 +0100

    add lovesek file

---
+++ Development/JatekosLovese.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Development
+{
+    class JatekosLovese
+    {
+        string name;
+        int x;
+        int y;
+        public int id { get; set; }
+
+        public JatekosLovese(string name, int x, int y, int id)
+        {
+            this.name = name;
+            this.x = x;
+            this.y = y;
+            this.id = id;
+        }
+
+        
+
+        
+
+        public int Y { get => y; set => y = value; }
+        public int X { get => x; set => x = value; }
+        public string Name { get => name; set => name = value; }
+
+    }
+}
