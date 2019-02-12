e3b42f012756c4c54d7d6d7d2c9c656902c7647d
Csókás László <cslaszlo01@gmail.com>
2019-02-12 21:09:03 +0100

    7. feladat

--- Development/JatekosLovese.cs
+++ Development/JatekosLovese.cs
@@ -9,11 +9,12 @@ namespace Development
     class JatekosLovese
     {
         string name;
-        int x;
-        int y;
+        double x;
+        double y;
         public int id { get; set; }
+        double distance;
 
-        public JatekosLovese(string name, int x, int y, int id)
+        public JatekosLovese(string name, double x, double y, int id)
         {
             this.name = name;
             this.x = x;
@@ -22,12 +23,16 @@ namespace Development
         }
 
         
-
-        
-
-        public int Y { get => y; set => y = value; }
-        public int X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+        public double X { get => x; set => x = value; }
         public string Name { get => name; set => name = value; }
-
+        public double Distance
+        {
+            get
+            {
+                return Math.Sqrt(x*x + y*y);
+            }
+            set => distance = value;
+        }
     }
 }
--- Development/Program.cs
+++ Development/Program.cs
@@ -10,6 +10,29 @@ namespace Development
     {
         static void Main(string[] args)
         {
+            List<JatekosLovese> list = ReadFile();
+            Console.WriteLine("5. feladat: Lövések száma: {0} db", list.Count.ToString());
+            
+            Console.ReadLine();
+        }
+
+        public static List<JatekosLovese> ReadFile()
+        {
+            string[] content = System.IO.File.ReadAllLines(@"C:\Users\Lackó\source\repos\Development\development\Development\lovesek.txt");
+            List<JatekosLovese> jatekoslovesek = new List<JatekosLovese>();
+            string name;
+            double x;
+            double y;
+
+            for (int i = 1; i < content.Length - 1; i++)
+            {
+                string[] datas = content[i].Split(';');
+                name = datas[0];
+                x = Convert.ToDouble(datas[1]);
+                y = Convert.ToDouble(datas[2]);
+                jatekoslovesek.Add(new JatekosLovese(name, x, y, i));
+            }
+            return jatekoslovesek;
         }
     }
 }
